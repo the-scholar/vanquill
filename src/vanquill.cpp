7a35de14897d75283a564ad0b580e75afb6d0bf7
the-scholar <the-scholar@dusttoash.org>
2023-10-26 15:05:33 -0500

    Update vanquill.cpp

--- src/vanquill.cpp
+++ src/vanquill.cpp
@@ -1,16 +1,58 @@
 #include <iostream>
 #include <windows.h>
 
-using namespace std;
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg) {
+	case WM_COMMAND:
+
+		break;
+
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+	return 0;
+}
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		LPSTR lpCmdLine, int nCmdShow) {
-	HWND hwnd =
-			CreateWindow(0, L"STATIC", L"Minimal Window", WS_OVERLAPPEDWINDOW, 100, 100, 640, 480, nullptr, nullptr, nullptr, nullptr);
+	const char *className = "TextInputWindowClass";
+
+	WNDCLASS wc = { };
+	wc.lpfnWndProc = WndProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = className;
+	RegisterClass(&wc);
+
+	HWND hwnd = CreateWindow(
+			className,
+			"Text Input Window",
+			WS_OVERLAPPEDWINDOW,
+			CW_USEDEFAULT, CW_USEDEFAULT, 400, 300,
+			NULL,
+			NULL,
+			hInstance,
+			NULL
+	);
+
+	// Create a text input control
+	CreateWindow(
+			"EDIT",
+			"",
+			WS_CHILD | WS_VISIBLE | WS_BORDER | ES_LEFT | ES_AUTOHSCROLL,
+			10, 10, 200, 25,
+			hwnd,
+			NULL,
+			hInstance,
+			NULL
+	);
+
 	ShowWindow(hwnd, nCmdShow);
 
-	MSG msg;
-	while (GetMessage(&msg, nullptr, 0, 0)) {
+	MSG msg = { };
+	while (GetMessage(&msg, NULL, 0, 0)) {
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
