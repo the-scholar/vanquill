7dafa5ed9bc735904ef38688353b815ed2e748c3
half-panther <145088865+half-panther@users.noreply.github.com>
2023-11-01 17:43:49 +0000

    add fps counter support

--- src/vanquill.cpp
+++ src/vanquill.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
 #include <windows.h>
 #include <wingdi.h>
+#include <chrono>
 
 namespace {
 HDC backBufferDC = nullptr;
 HBITMAP backBufferBitmap = nullptr;
+
+std::chrono::steady_clock::time_point startTime;
+int frameCount = 0;
+int currentFPS = 0;
+
+void UpdateFPS(HWND hwnd) {
+	frameCount++;
+
+	// Calculate elapsed time since the start
+	auto currentTime = std::chrono::steady_clock::now();
+	auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+			currentTime - startTime).count();
+
+	// Recalculate FPS every second
+	if (elapsedTime >= 30) {
+		currentFPS = frameCount;
+		frameCount = 0;
+		startTime = currentTime;
+
+		// Update the window title with FPS information
+		std::wstring title = L"Your Window Title - FPS: "
+				+ std::to_wstring(currentFPS*1000/30);
+		SetWindowTextW(hwnd, title.c_str());
+	}
+}
+
 /*
  * Draws a 54-px width, horizontal line (from right to left) at x, y
  */
@@ -139,6 +166,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 		BitBlt(hdc, 0, 0, clientRect.right, clientRect.bottom, backBufferDC, 0,
 				0, SRCCOPY);
 
+		UpdateFPS(hwnd);
+
 		EndPaint(hwnd, &ps);
 		return 0;
 	}
@@ -178,6 +207,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 			hInstance,
 			NULL
 	);
+
+	startTime = std::chrono::steady_clock::now();
+
 	ShowWindow(hwnd, nCmdShow);
 
 	MSG msg = { };
