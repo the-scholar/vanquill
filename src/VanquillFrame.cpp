6004e9c094df873ddcdbf0707ef83ae052e9e5d7
the-scholar <the-scholar@dusttoash.org>
2023-11-08 20:41:34 -0800

    Create VanquillFrame

---
+++ src/VanquillFrame.cpp
@@ -0,0 +1,18 @@
+/*
+ * VanquillFrame.cpp
+ *
+ *  Created on: Nov 8, 2023
+ *      Author: Zeale
+ */
+
+#include "VanquillFrame.h"
+
+VanquillFrame::VanquillFrame() {
+}
+
+VanquillFrame::~VanquillFrame() {
+}
+
+void VanquillFrame::draw(const HDC &hdc) {
+
+}
---
+++ src/VanquillFrame.h
@@ -0,0 +1,24 @@
+/*
+ * VanquillFrame.h
+ *
+ *  Created on: Nov 8, 2023
+ *      Author: Zeale
+ */
+
+#ifndef SRC_VANQUILLFRAME_H_
+#define SRC_VANQUILLFRAME_H_
+
+#include <windef.h>
+
+/*
+ * Class used to manage drawing of the window frame for Vanquill.
+ */
+class VanquillFrame {
+	int top = 30, left = 5, bottom = 5, right = 5;
+public:
+	VanquillFrame();
+	virtual ~VanquillFrame();
+	void draw(const HDC&);
+};
+
+#endif /* SRC_VANQUILLFRAME_H_ */
