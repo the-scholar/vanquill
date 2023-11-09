04afc1d075cbd2402fb237d599616964c891591e
the-scholar <the-scholar@dusttoash.org>
2023-11-08 21:18:45 -0800

    Update frame rendering

--- src/VanquillFrame.cpp
+++ src/VanquillFrame.cpp
@@ -44,12 +44,23 @@ void VanquillFrame::draw(HWND hwnd, WPARAM wParam, LPARAM lParam) const {
 		int height = rcWindow.bottom - rcWindow.top;
 
 		COLORREF borderColor = wParam ? 0 : 0x777777;
+		HBRUSH brush = CreateSolidBrush(borderColor);
 
-		HPEN hBorderPen = CreatePen(PS_SOLID, 3, borderColor);
+		HPEN hBorderPen = CreatePen(PS_SOLID, 30, borderColor);
 		HPEN hOldPen = (HPEN) SelectObject(hdc, hBorderPen);
 
 		// Draw the border to cover the entire non-client area
-		Rectangle(hdc, 0, 0, width, height);
+		RECT rect = { 0, 0, width, getTop() };
+		FillRect(hdc, &rect, brush); // Top
+
+		rect = { 0, 0, getLeft(), height };
+		FillRect(hdc, &rect, brush); // Left
+
+		rect = { 0, height - getBottom(), width, height };
+		FillRect(hdc, &rect, brush); // Bottom
+
+		rect = { width - getRight(), 0, width, height };
+		FillRect(hdc, &rect, brush); // Right
 
 		// Clean up
 		SelectObject(hdc, hOldPen);
